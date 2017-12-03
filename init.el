e623d04635c252e226acac96995f09db62d61b08
Ricardo Arredondo <ricardo.richo@gmail.com>
2017-12-02 22:49:13 -0600

    Copy line

--- init.el
+++ init.el
@@ -366,6 +366,31 @@ might be bad."
 (global-unset-key (kbd "M-k"))
 (global-set-key (kbd "M-k") 'kill-whole-line)
 
+;; Copy line
+(defun copy-line (arg)
+  "Copy lines (as many as prefix ARG) in the kill ring.
+Ease of use features:
+    - Move to start of next line.
+    - Appends the copy on sequential calls.
+    - Use newline as last char even on the last line of the buffer.
+    - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+;; optional key binding
+(global-set-key (kbd "C-c C-k") 'copy-line)
+
 ;; Reload file
 (defun my-reload-file ()
   "Reload current file by reverting current buffer."
